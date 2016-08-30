368913f194f18b4e859f6e8f8af95e6214923855
Paweł Bylica <chfast@gmail.com>
2016-08-30 15:35:52 +0200

    Improve EVMJIT Python interface

--- tests/test.py
+++ tests/test.py
@@ -1,26 +1,31 @@
 import hashlib
-from evmjit import EVMJIT, evm_mode
+from evmjit import EVMJIT, Env, evm_mode, to_uint256
 
 
-def query(env, key, arg):
-    print("query(env: {}, key: {}, arg: {})".format(env, key, arg))
-    if key == 0:
-        return 234588
+class TestEnv(Env):
+    def query(self, key, arg):
+        print("query(key: {}, arg: {})".format(key, arg))
+        if key == 0:
+            return 234588
+
+    def update(self, key, arg1, arg2):
+        print("update(key: {}, arg1: {}, arg2: {})".format(key, arg1, arg2))
+
+    def call(self, kind, gas, address, value, input, input_size, output,
+             output_size):
+        return False
 
 
-def update(env, key, arg1, arg2):
-    print("update(env: {}, key: {}, arg1: {}, arg2: {})"
-          .format(env, key, arg1, arg2))
-
-
-def call(env, kind, gas, address, value, input, input_size, output, output_size):
-    return False
+def test_to_uint256():
+    assert to_uint256(0)[0] == [0, 0, 0, 0]
+    assert to_uint256(1)[0] == [1, 0, 0, 0]
+    assert to_uint256(1 << 63)[0] == [1 << 63, 0, 0, 0]
+    assert to_uint256(1 << 64)[0] == [0, 1, 0, 0]
+    assert to_uint256(1 << 255)[0] == [0, 0, 0, 1 << 63]
 
 
 def test_evm():
-
-    evm = EVMJIT()
-    evm.evm_create(query, update, call)
+    jit = EVMJIT()
 
     code = b'6001600260035455'.decode('hex')
 
@@ -29,30 +34,29 @@ def test_evm():
     code_hash = h.digest()
 
     input = "Hello World!"
-
-    value_ffi = [1,0,0,0]
-
+    value = 4321543654643565
     gas = 200000
 
-    success = evm.set_option("hello", "world")
+    success = jit.set_option("hello", "world")
     assert not success
 
-    ready = evm.is_code_ready(evm_mode.EVM_HOMESTEAD, code_hash)
+    ready = jit.is_code_ready(evm_mode.EVM_HOMESTEAD, code_hash)
     assert not ready
 
-    evm.prepare_code(evm_mode.EVM_HOMESTEAD, code_hash, code)
+    jit.prepare_code(evm_mode.EVM_HOMESTEAD, code_hash, code)
 
-    ready = evm.is_code_ready(evm_mode.EVM_HOMESTEAD, code_hash)
+    ready = jit.is_code_ready(evm_mode.EVM_HOMESTEAD, code_hash)
     assert ready
 
-    # import pdb; pdb.set_trace()
-    env = {'name': "Env"}  # You can pass any Python object as env argument.
-    result = evm.evm_execute(env, evm_mode.EVM_HOMESTEAD, code_hash, code, gas,
-                             input, value_ffi)
+    env = TestEnv()
+    result = jit.execute(env, evm_mode.EVM_HOMESTEAD, code_hash, code, gas,
+                         input, value)
 
-    evm.evm_release_result(result)
-    evm.evm_destroy()
+    assert result.code == 0  # Success.
+    assert result.gas_left == 194941
+    assert result.output == b''
 
 
 if __name__ == "__main__":
+    test_to_uint256()
     test_evm()
